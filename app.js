ff7b7b6e04e492d3e155ed272e9571f13673533f
udayan-das11 <udayan.das11@gmail.com>
2016-07-24 14:41:00 +0530

    removed mysql

--- app.js
+++ app.js
@@ -4,14 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var mysql = require('mysql');
 
-var con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'bluelounge'
-  });
 
 var fs = require("fs");
 
